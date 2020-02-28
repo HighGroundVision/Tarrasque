2c776813844329a741a7af217ab445124f747d92
Jamie Webster <jamie.webster@factorybucket.com>
2020-02-28 10:37:33 -0500

    Applied the Same Blob Lease to the Abilities

--- HGV.Tarrasque.ProcessaAbilities/Services/AbilityService.cs
+++ HGV.Tarrasque.ProcessaAbilities/Services/AbilityService.cs
@@ -4,7 +4,10 @@ using HGV.Tarrasque.Common.Extensions;
 using HGV.Tarrasque.ProcessAbilities.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
+using Polly;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -39,26 +42,57 @@ namespace HGV.Tarrasque.ProcessAbilities.Services
         private static async Task<T> ReadData<T>(IBinder binder, BlobAttribute attr) where T : new()
         {
             var reader = await binder.BindAsync<TextReader>(attr);
-
-            T data;
             if (reader == null)
-            {
-                data = new T();
-            }
-            else
-            {
-                var input = await reader.ReadToEndAsync();
-                data = JsonConvert.DeserializeObject<T>(input);
-            }
+                throw new NullReferenceException(nameof(reader));
 
-            return data;
+            var input = await reader.ReadToEndAsync();
+            if (string.IsNullOrWhiteSpace(input))
+                throw new NullReferenceException(nameof(reader));
+
+            return JsonConvert.DeserializeObject<T>(input);
         }
 
-        private static async Task WriteData<T>(IBinder binder, BlobAttribute attr, T data)
+        private async Task ProcessBlob<T>(CloudBlockBlob blob, Action<T> updateFn, ILogger log) where T : new()
         {
-            var output = JsonConvert.SerializeObject(data);
-            var writer = await binder.BindAsync<TextWriter>(attr);
-            await writer.WriteAsync(output);
+            try
+            {
+                var exist = await blob.ExistsAsync();
+                if (!exist)
+                {
+                    var data = new T();
+                    var json = JsonConvert.SerializeObject(data);
+                    await blob.UploadTextAsync(json);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            var policy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryForeverAsync((n) => TimeSpan.FromMilliseconds(100));
+
+            var ac = await policy.ExecuteAsync(async () =>
+            {
+                var leaseId = await blob.AcquireLeaseAsync(TimeSpan.FromSeconds(30));
+                if (string.IsNullOrEmpty(leaseId))
+                    throw new NullReferenceException();
+
+                return AccessCondition.GenerateLeaseCondition(leaseId);
+            });
+
+            try
+            {
+                var input = await blob.DownloadTextAsync(ac, null, null);
+                var data = JsonConvert.DeserializeObject<T>(input);
+                updateFn(data);
+                var output = JsonConvert.SerializeObject(data);
+                await blob.UploadTextAsync(output, ac, null, null);
+            }
+            finally
+            {
+                await blob.ReleaseLeaseAsync(ac, null, null);
+            }
         }
 
         public async Task Process(Match match, IBinder binder, ILogger log)
@@ -84,92 +118,104 @@ namespace HGV.Tarrasque.ProcessAbilities.Services
             }
         }
 
-        private static async Task UpdateHistory(Match match, Player player, Ability ability, IBinder binder, ILogger log)
+        private async Task UpdateHistory(Match match, Player player, Ability ability, IBinder binder, ILogger log)
         {
             var attr = new BlobAttribute(string.Format(HISTORY_PATH, ability.Id));
-            var history = await ReadData<AbilityHistory>(binder, attr);
+            var blob = await binder.BindAsync<CloudBlockBlob>(attr);
 
-            var victory = match.Victory(player);
-            var ancestry = (ability.HeroId == player.hero_id);
-            var priority = player.PickPriority();
-            var date = DateTimeOffset.FromUnixTimeSeconds(match.start_time);
-            var bounds = date.AddDays(-6);
-            var mid = date.AddDays(-3);
+            Action<AbilityHistory> updateFn = (history) =>
+            {
+                var victory = match.Victory(player);
+                var ancestry = (ability.HeroId == player.hero_id);
+                var priority = player.PickPriority();
+                var date = DateTimeOffset.FromUnixTimeSeconds(match.start_time);
+                var bounds = date.AddDays(-6);
+                var mid = date.AddDays(-3);
 
-            history.Total.Picks++;
-            history.Total.Wins += victory ? 1 : 0;
-            history.Total.Ancestry += ancestry ? 1 : 0;
-            history.Total.Priority += priority;
+                history.Total.Picks++;
+                history.Total.Wins += victory ? 1 : 0;
+                history.Total.Ancestry += ancestry ? 1 : 0;
+                history.Total.Priority += priority;
 
-            history.Data.Add(new AbilityHistoryVictory() { Timestamp = date, Victory = victory, Ancestry = ancestry, Priority = priority });
-            history.Data.RemoveAll(_ => _.Timestamp < bounds);
+                history.Data.Add(new AbilityHistoryVictory() { Timestamp = date, Victory = victory, Ancestry = ancestry, Priority = priority });
+                history.Data.RemoveAll(_ => _.Timestamp < bounds);
 
-            var current = history.Data.Where(_ => _.Timestamp > mid).ToList();
-            history.Current.Picks = current.Count();
-            history.Current.Wins = current.Count(_ => _.Victory == true);
-            history.Current.Ancestry = current.Count(_ => _.Ancestry == true);
-            history.Current.Priority = current.Sum(_ => _.Priority);
+                var current = history.Data.Where(_ => _.Timestamp > mid).ToList();
+                history.Current.Picks = current.Count();
+                history.Current.Wins = current.Count(_ => _.Victory == true);
+                history.Current.Ancestry = current.Count(_ => _.Ancestry == true);
+                history.Current.Priority = current.Sum(_ => _.Priority);
 
-            var previous = history.Data.Where(_ => _.Timestamp < mid).ToList();
-            history.Previous.Picks = previous.Count;
-            history.Previous.Wins = previous.Count(_ => _.Victory == true);
-            history.Previous.Ancestry = previous.Count(_ => _.Ancestry == true);
-            history.Previous.Priority = previous.Sum(_ => _.Priority);
-
-            await WriteData(binder, attr, history);
+                var previous = history.Data.Where(_ => _.Timestamp < mid).ToList();
+                history.Previous.Picks = previous.Count;
+                history.Previous.Wins = previous.Count(_ => _.Victory == true);
+                history.Previous.Ancestry = previous.Count(_ => _.Ancestry == true);
+                history.Previous.Priority = previous.Sum(_ => _.Priority);
+            };
+            await ProcessBlob(blob, updateFn, log);
         }
 
-        private static async Task UpdateDetails(Match match, Player player, Ability ability, List<Ability> abilities, IBinder binder, ILogger log)
+        private async Task UpdateDetails(Match match, Player player, Ability ability, List<Ability> abilities, IBinder binder, ILogger log)
         {
             var attr = new BlobAttribute(string.Format(DETAILS_PATH, ability.Id));
-            var details = await ReadData<AbilityCombo>(binder, attr);
+            var blob = await binder.BindAsync<CloudBlockBlob>(attr);
 
-            var victory = match.Victory(player);
-
-            // Hero Combo
+            Action<AbilityCombo> updateFn = (details) =>
             {
-                var item = details.Heroes.Find(_ => _.Id == player.hero_id);
-                if (item == null)
+                var victory = match.Victory(player);
+
+                // Hero Combo
                 {
-                    item = new AbilityComboStat() { Id = player.hero_id };
-                    details.Heroes.Add(item);
+                    var item = details.Heroes.Find(_ => _.Id == player.hero_id);
+                    if (item == null)
+                    {
+                        item = new AbilityComboStat() { Id = player.hero_id };
+                        details.Heroes.Add(item);
+                    }
+
+                    item.Update(victory);
                 }
 
-                item.Update(victory);
-            }
-
-            // Ability Combos
-            foreach (var a in abilities)
-            {
-                if (a.Id == ability.Id)
-                    continue;
-
-                var item = details.Abilities.Find(_ => _.Id == a.Id);
-                if (item == null)
+                // Ability Combos
+                foreach (var a in abilities)
                 {
-                    item = new AbilityComboStat() { Id = a.Id };
-                    details.Abilities.Add(item);
+                    if (a.Id == ability.Id)
+                        continue;
+
+                    var item = details.Abilities.Find(_ => _.Id == a.Id);
+                    if (item == null)
+                    {
+                        item = new AbilityComboStat() { Id = a.Id };
+                        details.Abilities.Add(item);
+                    }
+
+                    item.Update(victory);
                 }
-
-                item.Update(victory);
-            }
-
-            await WriteData(binder, attr, details);
+            };
+            await ProcessBlob(blob, updateFn, log);
         }
 
         public async Task UpdateSummary(IBinder binder, ILogger log)
         {
-            var attr = new BlobAttribute(SUMMARY_PATH);
-            var summary = await ReadData<AbilitySummary>(binder, attr);
+            var blob = await binder.BindAsync<CloudBlockBlob>(new BlobAttribute(SUMMARY_PATH));
 
+            var data = new Dictionary<int, AbilityHistoryData>();
             var abilities = this.metaClient.GetSkills().Where(_ => _.AbilityDraftEnabled).ToList();
             foreach (var ability in abilities)
             {
-                var history = await ReadData<AbilityHistory>(binder, new BlobAttribute(string.Format(HISTORY_PATH, ability.Id)));
-                summary.Data[ability.Id] = history;
+                try
+                {
+                    var attr = new BlobAttribute(string.Format(HISTORY_PATH, ability.Id));
+                    var history = await ReadData<AbilityHistory>(binder, attr);
+                    data.Add(ability.Id, history);
+                }
+                catch (Exception)
+                {
+                }
             }
 
-            await WriteData(binder, attr, summary);
+            Action<AbilitySummary> updateFn = (summary) => summary.Data = data;
+            await ProcessBlob(blob, updateFn, log);
         }
 
         public async Task<List<DTO.AbilitySummary>> GetSummary(IBinder binder, ILogger log)
